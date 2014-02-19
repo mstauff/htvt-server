ffc109dfc7a5820bb55ea5160a52c9e8ea5b04a0
Matt Stauffer <matt.stauffer@ldschurch.org>
2014-02-19 16:55:50 -0700

    Added basic URLs

--- routes/index.js
+++ routes/index.js
@@ -9,14 +9,29 @@ exports.index = function(req, res){
 
 exports.mobileConfig = function(req, res){
 
+    var baseUrl = "http://htvt-ldscd.rhcloud.com/";
+    var baseHtvtEndpoint = baseUrl + "htvt/services/v1/"
+    var districtsUrl = baseHtvtEndpoint + "%@/districts/"
+    var compUrl = baseHtvtEndpoint + "companionships/"
+    var visitUrl = baseHtvtEndpoint + "%@/visits/"
     var config = {
         urls : {
             "login":"lds.org/signin",
             "logout":"lds.org/signout",
-            "member_list":"lds.org/directory",
-            "htvt_districts":"https://beta.lds.org/htvt/%@/districts/%@"
+            "current_user": baseHtvtEndpoint + "user",
+            "member_list": baseHtvtEndpoint + "%@/members",
+            "htvt_districts": districtsUrl + "%@",
+            "district_create": districtsUrl,
+            "district_update": districtsUrl + "%@",
+            "district_delete": districtsUrl + "%@",
+            "comp_create": compUrl,
+            "comp_delete": compUrl + "%@",
+            "visit_record": visitUrl + "record",
+            "visit_delete": visitUrl + "%@"
+            "latest_visits": visitUrl + "latestByOrganization/%@"
 
-        }
+        },
+        cacheMemberData : "true"
     }
   res.send(config);
 };
